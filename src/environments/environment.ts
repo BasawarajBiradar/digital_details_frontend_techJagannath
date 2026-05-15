44af5fe76e64f7a4039c1a922455bdb84956faaa
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-05-15 17:23:05 +0530

    updated environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080'  // your Spring Boot port
+  // apiUrl: 'http://192.168.1.58:8080'  // your Spring Boot port
   apiUrl: 'https://api.techjagannath.com'
 };
