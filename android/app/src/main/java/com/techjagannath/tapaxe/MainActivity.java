2cab2f53e60f61d9d05997016290459656bbece0
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-07-30 09:40:36 +0530

    added android and ios dependencies

---
+++ android/app/src/main/java/com/techjagannath/tapaxe/MainActivity.java
@@ -0,0 +1,5 @@
+package com.techjagannath.tapaxe;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.techjagannath.tapaxe',
+  appName: 'TAPAXE',
+  webDir: 'dist/digital-identification-techjagannath/browser'
+};
+
+export default config;
